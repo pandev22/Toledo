18df4030de214b479052ca96dff9ebffba2906fe
achul123 <thetheatregaming@gmail.com>
2026-03-18 14:20:56 +0100

    update(routing): add HTML caching and file watcher for hot-reload

--- server/modules/routing.js
+++ server/modules/routing.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
+const chokidar = require('chokidar');
 const loadConfig = require('../handlers/config.js');
 const settings = loadConfig('./config.toml');
 
@@ -27,33 +28,63 @@ const HeliactylModule = {
 module.exports.HeliactylModule = HeliactylModule;
 module.exports.load = async function (app, db) {
   const distPath = path.join(__dirname, '../../frontend/dist');
+  const indexPath = path.join(distPath, 'index.html');
+  const devIndexPath = path.join(__dirname, '../../frontend/index.html');
+
+  let cachedHtml = null;
+  let isUsingDistBuild = false;
+
+  const renderHtml = (html) => html.replace(/{{SITE_NAME}}/g, settings.website.name || 'Heliactyl');
+
+  async function refreshHtmlCache() {
+    try {
+      const html = await fs.readFile(indexPath, 'utf8');
+      cachedHtml = renderHtml(html);
+      isUsingDistBuild = true;
+      return cachedHtml;
+    } catch (distError) {
+      const html = await fs.readFile(devIndexPath, 'utf8');
+      cachedHtml = renderHtml(html);
+      isUsingDistBuild = false;
+      return cachedHtml;
+    }
+  }
+
+  await refreshHtmlCache().catch(() => null);
+
+  const watcher = chokidar.watch([indexPath, devIndexPath], {
+    ignoreInitial: true,
+    awaitWriteFinish: {
+      stabilityThreshold: 150,
+      pollInterval: 50
+    }
+  });
+
+  watcher.on('add', () => refreshHtmlCache().catch(() => null));
+  watcher.on('change', () => refreshHtmlCache().catch(() => null));
+  watcher.on('unlink', () => refreshHtmlCache().catch(() => null));
 
   app.use('/', express.static(distPath, {
     fallthrough: true,
-    index: false
+    index: false,
+    etag: false,
+    lastModified: false,
+    setHeaders: (res) => {
+      res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+    }
   }));
 
   app.get('/*', async (req, res, next) => {
     if (req.path.startsWith('/api/')) return next();
 
     try {
-      const indexPath = path.join(distPath, 'index.html');
-      let html = await fs.readFile(indexPath, 'utf8');
+      const html = cachedHtml || await refreshHtmlCache();
 
-      // Inject dynamic values
-      html = html.replace(/{{SITE_NAME}}/g, settings.website.name || "Heliactyl");
-
+      res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+      res.setHeader('X-Frontend-Source', isUsingDistBuild ? 'dist' : 'dev');
       res.send(html);
-    } catch (err) {
-      // If dist/index.html doesn't exist, try frontend/index.html (dev mode)
-      try {
-        const devIndexPath = path.join(__dirname, '../../frontend/index.html');
-        let html = await fs.readFile(devIndexPath, 'utf8');
-        html = html.replace(/{{SITE_NAME}}/g, settings.website.name || "Heliactyl");
-        res.send(html);
-      } catch (devErr) {
-        next();
-      }
+    } catch (error) {
+      next();
     }
   });
 };
